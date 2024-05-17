6689d4f9efa9f187d0589cf57d19cc17575024bc
Kelvin Bobsin <kelvin.bobsin.1999@gmail.com>
2024-05-16 22:41:35 -0300

    ...

--- cypress.config.js
+++ cypress.config.js
@@ -4,13 +4,14 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://lojaebac.ebaconline.art.br/',
     setupNodeEvents(on, config) {
-      require('cypress-html-reporter/GenerateReport')(on, config)
+
     },
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportFilename: "[name]-result",
+      html: true
+    }
   },
-  
-  // reporter: 'mochawesome',
-  // reporterOptions: {
-  //   reportFilename: "[name]-result",
-  //   html: false
-  // }
+
+
 });
